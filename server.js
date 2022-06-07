dcb5ab1049f321a6dab520ba80c47a6d474673ec
Merieme Elhafidi <mimi.elhafidi99@gmail.com>
2022-06-07 15:30:27 +0200

    push changes fix heroku problems

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
+var dbConn = require('../../config/db.config');
 
 let port = process.env.PORT || 8080
 
@@ -11,23 +12,13 @@ app.use(bodyParser.urlencoded({extended: false}))
 // parse request data content type application/json
 app.use(bodyParser.json());
 
-// Connection Details 
-const connection = mysql.createConnection({
-    host: 'eu-cdbr-west-02.cleardb.net',
-    user: 'b39de166698829',
-    password: 'bfd17a8b',
-    database: 'heroku_23ed422d7cab436'
-})
-
-
 // View engine
 app.set('view engine', 'ejs')
-
 
 // Render Home page
 app.get('/', function(req, res) {
 
-    connection.query('SELECT * FROM Section', (error, rows) => {
+    dbConn.query('SELECT * FROM Section', (error, rows) => {
         if(error) throw error;
     
         if(!error) {
